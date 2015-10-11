2983361fa39ac04523d6f5a450420b9b41cd28df
lolstorm <ricardocampos65@hotmail.com>
2015-10-11 03:34:48 +0100

    More Functions

--- proj.lisp
+++ proj.lisp
@@ -9,11 +9,31 @@ coluna peca)
 ;; Tipo tabuleiro
 
 (defstruct
-	(tabuleiro)
+	(tabuleiro (:conc-name tr-))
 tab)
 
+;; Tipo Problema
+(defstruct problema
+	estado-inicial
+	solucao
+	accoes
+	resultado
+	custo-caminho
+)
 
+;Tipo Tabuleiro
 (defun cria-tabuleiro ()
 	(make-tabuleiro :tab (make-array (list 18 10)))
 )
 
+(defun copia-tabuleiro (tabuleiro)
+	(copy-seq tabuleiro)
+)
+
+(defun tabuleiro-preenchido-p (tabuleiro linha coluna):
+	(aref (tr-tab tabuleiro) linha coluna)
+)
+
+(defun tabuleiro-altura-coluna (tabuleiro coluna):
+	
+)
