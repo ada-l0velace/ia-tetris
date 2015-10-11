54129053e79f9738e4f04c9b00e6931a06216430
lolstorm <ricardocampos65@hotmail.com>
2015-10-11 16:16:56 +0100

    added more function to type tabuleiro

--- proj.lisp
+++ proj.lisp
@@ -1,3 +1,5 @@
+ (defparameter *dim-linhas* 18)
+ (defparameter *dim-colunas* 10)
 ;; Tipo accao
 
 (defstruct
@@ -23,7 +25,7 @@ tab)
 
 ;;Tipo Tabuleiro
 (defun cria-tabuleiro ()
-	(make-tabuleiro :tab (make-array (list 18 10)))
+	(make-tabuleiro :tab (make-array (list *dim-linhas* *dim-colunas*)))
 )
 
 ;des
@@ -61,5 +63,37 @@ tab)
 		)
 	)
 	T)
+)
+
+(defun tabuleiro-preeche! (tabuleiro linha coluna)
+	(if (not (or (> linha *dim-linhas*) (> coluna *dim-colunas*)))
+		(setf (aref (tr-tab tabuleiro) linha coluna) T)
+	)
+)
+
+(defun tabuleiro-remove-linha! (tabuleiro linha)
+	(loop for i from 0 below *dim-colunas* do
+		(setf (aref (tr-tab tabuleiro) linha i) NIL)
+	)
+)
+
+(defun tabuleiro-topo-preenchido-p (tabuleiro)
+	(loop for i from 0 below *dim-colunas* do
+		(if (eq (tabuleiro-preenchido-p tabuleiro (1- *dim-linhas*) i) T)
+			(return-from tabuleiro-topo-preenchido-p T)
+		)
+	)
+	NIL	
+)
+
+(defun tabuleiros-iguais-p (tabuleiro tabuleiro1)
+	(loop for i from 0 below *dim-linhas* do
+		(loop for j from 0 below *dim-colunas* do
+			(if (not (eq (tabuleiro-preenchido-p tabuleiro i j) (tabuleiro-preenchido-p tabuleiro1 i j)))
+				(return-from tabuleiros-iguais-p NIL)
+			)
+		)
+	)
+	T	
 )
 
