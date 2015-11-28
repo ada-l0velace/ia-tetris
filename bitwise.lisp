f8ba234e4b700d1d22a5f6182453636a3fa61cbb
lolstorm <ricardocampos65@hotmail.com>
2015-11-28 02:30:52 +0000

    last version before wipe out

--- bitwise.lisp
+++ bitwise.lisp
@@ -1,18 +1,173 @@
 (defparameter *dim-linhas* 18)
 (defparameter *dim-colunas* 10)
+(defparameter *grid-mask* (ash 1 *dim-colunas*))
+(defconstant *tabuleiro-mascaras* #(512 256 128 64 32 16 8 4 2 1))
+(defstruct tabuleiro (:conc-name tr-)
+	tab
+	alturas
+)
 
+(defun logbit (integer index)
+	(ldb (byte 1 index) integer))
+ 
 (defun cria-tabuleiro ()
-	(mapcar #'(lambda (x) (declare (ignore x))(make-array (list *dim-colunas*) :element-type '(mod 2) :initial-element 0)) (make-list *dim-linhas*)))
+	(make-tabuleiro 
+		:tab (make-array (list *dim-linhas*) :initial-element 0)
+		:alturas (make-array (list *dim-colunas*) :initial-element 0)
+	)
+)
+
+(defun copia-tabuleiro (tabuleiro)
+	(make-tabuleiro 
+		:tab (tabuleiro->array tabuleiro)
+		:alturas (copy-seq (tr-alturas tabuleiro))
+	)
+)
 
 (defun tabuleiro-preenchido-p (tabuleiro linha coluna)
-	(aref (nth linha tabuleiro) (- 9 coluna)))
+	(if (> (logbit (aref (tabuleiro-tab tabuleiro) linha) (- (1- *dim-colunas*) coluna)) 0)
+		T
+		NIL
+	)
+)
 
-(defun tabuleiro-linha (tabuleiro linha)
-	(nth linha tabuleiro))
+(defun tabuleiro-linha-p (tabuleiro linha)
+	(aref (tabuleiro-tab tabuleiro) linha))
+
+
+
+(defun tabuleiro-preenche! (tabuleiro linha coluna)
+	(when (and (<= 0 linha (1- *dim-linhas*)) (<= 0 coluna (1- *dim-colunas*)))
+		(setf (aref (tabuleiro-tab tabuleiro) linha) (logior (aref (tabuleiro-tab tabuleiro) linha) (aref *tabuleiro-mascaras* coluna)))
+		(when (> (1+ linha) (tabuleiro-altura-coluna tabuleiro coluna))
+			(tabuleiro-altura! tabuleiro coluna (1+ linha))
+		)
+	)
+T)
+
+(defun tabuleiro-muda-ponto! (tabuleiro linha coluna valor) 
+	(if (eq valor T)
+		(tabuleiro-preenche! tabuleiro linha coluna)
+		(tabuleiro-apaga! tabuleiro linha coluna)
+	)
+T)
+
+(defun tabuleiro-apaga! (tabuleiro linha coluna)
+	(when (and (< 0 linha (1- *dim-linhas*)) (< 0 coluna (1- *dim-colunas*)))
+		(setf (aref (tabuleiro-tab tabuleiro) linha) (logand (aref (tabuleiro-tab tabuleiro) linha) (lognot (aref *tabuleiro-mascaras* coluna)))) 
+		(when (<= linha (tabuleiro-altura-coluna tabuleiro coluna))			
+			(tabuleiro-altura! tabuleiro coluna (tabuleiro-calcula-altura tabuleiro coluna linha))
+		)
+	)
+T)
+
+(defun tabuleiro-altura-coluna (tabuleiro coluna)
+	(aref (tr-alturas tabuleiro) coluna))
+
+(defun tabuleiro-altura! (tabuleiro coluna altura)
+	(setf (aref (tr-alturas tabuleiro) coluna) altura)
+T)
+
+(defun tabuleiro-calcula-altura (tabuleiro coluna topo)
+	(let (
+		(altura 0)
+	)
+	(loop for i downfrom topo downto 0 do
+		(if (null (tabuleiro-preenchido-p tabuleiro i coluna))
+			(incf altura)
+			(return-from tabuleiro-calcula-altura (1+ (- topo altura)))
+		)
+	)
+	0) ; coluna vazia
+)
+
+;; tabuleiro-desce-peca: tabuleiro x peca x coluna --> inteiro
+;; esta funcao calcula a altura ate onde a peca pode descer ate encontrar o chao
+;; ou um bloco preenchido onde pousar
+(defun tabuleiro-desce-peca (tabuleiro peca coluna)
+	(let* (
+		(dim-linhas-peca (peca-dimensao-altura peca))
+		(drop (- *dim-linhas* dim-linhas-peca))
+	)
+		(loop for l downfrom drop downto 0 do
+			(if (null (tabuleiro-peca-pode-descer-p tabuleiro peca l coluna))
+				(return-from tabuleiro-desce-peca (+ 1 l))
+			)
+		)
+	0)
+	
+)
+
+;; tabuleiro-peca-pode-descer-p: tabuleiro x peca x linha x coluna --> logico
+;; esta funcao recebe um tabuleiro, uma peca, a linha e uma coluna e verifica
+;; se ao longo da sua largura a peca pode descer ou nao retorna verdadeiro se pode
+;; retorna falso se nao
+(defun tabuleiro-peca-pode-descer-p (tabuleiro peca linha coluna)
+	(let (
+		(dim-linhas-peca (peca-dimensao-altura peca))
+		(dim-colunas-peca (peca-dimensao-largura peca))
+		(_pc 0)
+		(_pl 0)
+	)
+	
+		(if (>= (+ linha dim-linhas-peca) *dim-linhas*)
+			(return-from tabuleiro-peca-pode-descer-p T)
+		)	
+	
+		(loop for pl downfrom (1- dim-linhas-peca) downto 0 do
+			(loop for pc from 0 below dim-colunas-peca do
+				(setf _pl (+ linha pl 1))
+				(setf _pc (+ coluna pc))
+				(if (and (eq (aref peca pl pc) T) (>= _pl 0))
+					(if (not (and (< _pl *dim-linhas*) (null (tabuleiro-preenchido-p tabuleiro (1- _pl) _pc))))
+						(return-from tabuleiro-peca-pode-descer-p NIL)
+					)
+				)		
+			)
+		)
+		(return-from tabuleiro-peca-pode-descer-p T)
+	)
+)
+
+(defun tabuleiros-iguais-p (tabuleiro tabuleiro1)
+	(equalp tabuleiro tabuleiro1)	
+)
+
+;; tabuleiro->array: tabuleiro --> array
+;; este transformador de saida recebe um tabuleiro e devolve um novo array igual ao array do tabuleiro recebido
+(defun tabuleiro->array (tabuleiro)
+	(let (
+		(new-array (make-array (list *dim-linhas*) :initial-element 0))
+	)
+	(loop for i from 0 below *dim-linhas* do
+		(loop for j from 0 below *dim-colunas* do
+			(if (tabuleiro-preenchido-p tabuleiro i j)
+				(setf (aref new-array i j) (aref (tr-tab tabuleiro) i j))
+			)		
+		)
+	)
+	new-array)	
+)
+
+;; array->tabuleiro: array --> tabuleiro
+;; este transformador de saida recebe um array e controi um novo tabuleiro com o conteudo do array recebido e
+;; devolve um tabuleiro 
+(defun array->tabuleiro (array)
+	(let (
+		(new-tabuleiro (cria-tabuleiro))
+	)
+	(loop for i from 0 below *dim-linhas* do
+		(loop for j from 0 below *dim-colunas* do
+			(if (aref array i j)
+				(tabuleiro-muda-ponto! new-tabuleiro i j (aref array i j))		
+			)
+		)
+	)
+	new-tabuleiro)
+)
 
 (defun tabuleiro-buracos (tabuleiro)
 	(let (
-		(grid-mask (ash (make-array (list *dim-colunas*) :element-type '(mod 2) :initial-contents #*0000000001) *dim-colunas*))
 		(under-mask 0)
 		(l-neighbor-mask 0)
 		(r-neighbor-mask 0)
@@ -25,21 +180,21 @@
 		(loop while 
 			(and 
 				(< min-y *dim-linhas*) 
-				(eq (tabuleiro-linha tabuleiro min-y) grid-mask)
+				(eq (tabuleiro-linha-p tabuleiro min-y) *grid-mask*)
 			) 
 		do 
 			(incf min-y)
 		)
 		;(format T "~d~c" grid-mask #\linefeed)
 		(loop for y from min-y below *dim-linhas* do
-			(setf line (tabuleiro-linha tabuleiro y))
-			(setf filled (bit-and (bit-not line) grid-mask))
+			(setf line (tabuleiro-linha-p tabuleiro y))
+			(setf filled (logand (lognot line) *grid-mask*))
 			;(format T "~d~c" under-mask #\linefeed)
-			(setf under-mask (bit-ior under-mask filled))
+			(setf under-mask (logior under-mask filled))
 			;(format T "~d~c" filled #\linefeed)
 			;(format T "~d~c" under-mask #\linefeed)
-			(setf l-neighbor-mask (bit-ior l-neighbor-mask (ash filled 1)))
-			(setf r-neighbor-mask (bit-ior r-neighbor-mask (ash filled -1)))
+			(setf l-neighbor-mask (logior l-neighbor-mask (ash filled 1)))
+			(setf r-neighbor-mask (logior r-neighbor-mask (ash filled -1)))
 			;(format T "~d~c" (integer-length (logand r-neighbor-mask line)) #\linefeed)
 			(incf found-holes 
 				(+
@@ -51,4 +206,3 @@
 		)
 	found-holes)
 )
-
